6118bd2dc83e48dc03024a7abe80c24985181cde
Ocklaf <ocklaf@gmail.com>
2023-07-20 13:44:09 +0200

    Exercise 1, unit 2, Open Bootcamp

---
+++ Ex1/SystemOutDataTypes/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(1);
+        System.out.println(1.22);
+        System.out.println(2.121d);
+        System.out.println(true);
+    }
+}
